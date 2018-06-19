94a36661fa26cd474fc4b541a8889256c121943c
Neeraj Prasad <nprasad@mit.edu>
2018-06-19 08:59:20 -0700

    priority queue

--- launch.sh
+++ launch.sh
@@ -2,8 +2,9 @@
 #SBATCH -n 4
 #SBATCH --job-name=autoencoder
 #SBATCH --mem=10GB
-#SBATCH -t 04:30:00
+#SBATCH -t 02:30:00
 #SBATCH --workdir=/om/user/nprasad/aesap/subs/
+#SBATCH --qos=cbmm
 
 singularity exec -B /om:/om --nv /om/user/nprasad/singularity/xboix-localtensorflow.img \
 python /om/user/nprasad/aesap/main.py /om/user/nprasad/aesap/
