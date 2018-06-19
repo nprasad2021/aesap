73cbf0e5a66f78dfcfbd6f49da4b4c6053231945
Neeraj Prasad <nprasad@mit.edu>
2018-06-19 09:56:48 -0700

    add support for conda

---
+++ launch_conda.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH -n 4
+#SBATCH --job-name=autoencoder
+#SBATCH --mem=10GB
+#SBATCH -t 02:30:00
+#SBATCH --workdir=/om/user/nprasad/aesap/subs/
+#SBATCH --qos=cbmm
+
+python /om/user/nprasad/aesap/main.py /om/user/nprasad/aesap/
