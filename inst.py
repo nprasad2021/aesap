8c29aa3d82b52fb011944d739357e9f0c77d1d8f
Neeraj Prasad <nprasad@mit.edu>
2018-06-28 07:16:51 -0700

    asd

--- inst.py
+++ inst.py
@@ -42,9 +42,9 @@ def gen_tune_exp(precursor):
 										opt[-1].scale = sc
 										opt[-1].slide = sl
 										opt[-1].batch_size = b
-										opt[-1].build = bill
+										opt[-1].buiald = bill
 										opt[-1].restart = True
-										opt[-1].mode = 'both'
+										opt[-1].mode = 'test'
 										opt[-1].num_units = hidden
 										opt[-1].category = 'visual/'
 										opt[-1].loss = l
