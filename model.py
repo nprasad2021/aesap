b21eb0a15141d5bd3dd1aac77825b10169006510
Neeraj Prasad <nprasad@mit.edu>
2018-06-28 00:01:09 -0700

    checker

--- model.py
+++ model.py
@@ -346,9 +346,9 @@ class Autoencoder(object):
                     labels_ref = label
                     start = False
                 else:
-                    latent_codes_ref = np.concatenate(latent_codes_ref, latent_code)
-                    addrs_codes_ref = np.concatenate(addrs_codes_ref, addrs_code)
-                    labels_ref = np.concatenate(labels_ref, label)
+                    latent_codes_ref = np.concatenate(latent_codes_ref, latent_code, axis=0)
+                    addrs_codes_ref = np.concatenate(addrs_codes_ref, addrs_code, axis=0)
+                    labels_ref = np.concatenate(labels_ref, label, axis=0)
             except:
                 print('dataset iteration finished')
                 break
